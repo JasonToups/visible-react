8ae539e7f7e644a947f5700814df0e8cdc6498c4
jasontoups <me@jasontoups.com>
2024-10-22 12:55:09 -0700

    updates for build failure

--- src/components/pages/getting-started/DeployToGitHubPages.tsx
+++ src/components/pages/getting-started/DeployToGitHubPages.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Inspect from '../../common/Inspect';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
